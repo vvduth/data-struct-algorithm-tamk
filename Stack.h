fa264fc6069c1942dab95c955eae5d84db3355c1
vvduth <ducthai060501@gmail.com>
2021-10-06 09:25:33 +0300

    Stack and queue updated

--- Stack.h
+++ Stack.h
@@ -7,15 +7,22 @@ using namespace std;
 typedef double Stack_entry;
 const int maxstack = 10 ;
 
-class Stack {
+class Stack
+{
 public:
-	Stack();
-	bool empty() const;
-	Error_code pop();
-	Error_code top(Stack_entry &item) const;
-	Error_code push(const Stack_entry &item);
+    Stack();
+    bool empty() const;
+    Error_code pop();
+    Error_code top(Stack_entry& item) const;
+    Error_code push(const Stack_entry& item);
 
 private:
-	int count;
-	Stack_entry entry[10];
+    int count;
+    Stack_entry entry[maxstack];
+
+    friend Error_code copy_stack_c(Stack& dest, const Stack& source);
 };
+
+
+// Friend function to copy stack
+Error_code copy_stack_c(Stack& dest, const Stack& source);
